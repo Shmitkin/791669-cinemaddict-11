28ff07267ca5ba4a818aea7394ee3a6788b5a0a3
Shmitkin <mitkinoleg@gmail.com>
2020-04-06 15:04:09 +0300

    Добавил отображение карточки фильма, переписал повторяющийся код

--- src/components/stat.js
+++ src/components/stat.js
@@ -1,4 +1,4 @@
-export const createFooterStatisticsTemplate = () => {
+export const createStatTemplate = () => {
   return (
     `<p>130 291 movies inside</p>`
   );
--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import {createFilmCardTemplate} from "./components/film-card.js";
 import {createFilmsTemplate} from "./components/films.js";
-import {createFooterStatisticsTemplate} from "./components/footer-statistics.js";
+import {createStatTemplate} from "./components/stat.js";
 import {createHeaderProfileTemplate} from "./components/header-profile.js";
-import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
+import {createShowMoreButtonTemplate} from "./components/show-more-button.js";
 import {createMainNavigationTemplate} from "./components/main-navigation.js";
-import {createMostCommentedFilmsTemplate} from "./components/most-commented-films.js";
-import {createSortTemplate} from "./components/sorting.js";
-import {createTopRatedFilmsTemplate} from "./components/top-rated-films.js";
+import {createSortTemplate} from "./components/sort.js";
+import {createFilmsListTemplate} from "./components/films-list.js";
 
-const CardsCount = {
+import {createFilmDetailsTemplate} from "./components/film-details.js";
+import {createFilmInfoTemplate} from "./components/film-info.js";
+import {createFilmControlsTemplate} from "./components/film-controls.js";
+import {createCommentTemplate} from "./components/comment.js";
+import {createNewCommentTemplate} from "./components/new-comment.js";
+
+
+const CardCount = {
   MAIN: 5,
   TOP_RATED: 2,
   MOST_COMMENTED: 2
 };
+
+const COMMENT_COUNT = 4;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -20,7 +28,8 @@ const render = (container, template, place = `beforeend`) => {
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
-const footerStaticticsElement = document.querySelector(`.footer__statistics`);
+const siteFooterElement = document.querySelector(`.footer`);
+const footerStaticticsElement = siteFooterElement.querySelector(`.footer__statistics`);
 
 render(siteHeaderElement, createHeaderProfileTemplate());
 render(siteMainElement, createMainNavigationTemplate());
@@ -28,26 +37,46 @@ render(siteMainElement, createSortTemplate());
 render(siteMainElement, createFilmsTemplate());
 
 const filmsElement = siteMainElement.querySelector(`.films`);
+
+render(filmsElement, createFilmsListTemplate(``, `All movies. Upcoming`, `visually-hidden`));
+
 const filmsListElement = filmsElement.querySelector(`.films-list`);
 const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
 
-for (let i = 0; i < CardsCount.MAIN; i++) {
+for (let i = 0; i < CardCount.MAIN; i++) {
   render(filmsListContainerElement, createFilmCardTemplate());
 }
 
-render(filmsListElement, createLoadMoreButtonTemplate());
-render(filmsElement, createTopRatedFilmsTemplate());
-render(filmsElement, createMostCommentedFilmsTemplate());
+render(filmsListElement, createShowMoreButtonTemplate());
+render(filmsElement, createFilmsListTemplate(`--extra`, `Top rated`));
+render(filmsElement, createFilmsListTemplate(`--extra`, `Most commented`));
 
 const topRatedListElement = filmsElement.querySelector(`.films-list--extra .films-list__container`);
 const mostCommentedListElement = filmsElement.querySelector(`.films-list--extra:last-child .films-list__container`);
 
-for (let i = 0; i < CardsCount.TOP_RATED; i++) {
+for (let i = 0; i < CardCount.TOP_RATED; i++) {
   render(topRatedListElement, createFilmCardTemplate());
 }
 
-for (let i = 0; i < CardsCount.MOST_COMMENTED; i++) {
+for (let i = 0; i < CardCount.MOST_COMMENTED; i++) {
   render(mostCommentedListElement, createFilmCardTemplate());
 }
 
-render(footerStaticticsElement, createFooterStatisticsTemplate());
+render(footerStaticticsElement, createStatTemplate());
+
+render(siteFooterElement, createFilmDetailsTemplate(COMMENT_COUNT), `afterend`);
+
+const filmDetailsElement = document.querySelector(`.film-details`);
+const filmDetailsTopContainerElement = filmDetailsElement.querySelector(`.form-details__top-container`);
+const filmDetailsCommentsElement = filmDetailsElement.querySelector(`.film-details__comments-wrap`);
+const filmDetailsCommentsListElement = filmDetailsCommentsElement.querySelector(`.film-details__comments-list`);
+
+render(filmDetailsTopContainerElement, createFilmInfoTemplate());
+render(filmDetailsTopContainerElement, createFilmControlsTemplate());
+render(filmDetailsCommentsElement, createNewCommentTemplate());
+
+
+for (let i = 0; i < COMMENT_COUNT; i++) {
+  render(filmDetailsCommentsListElement, createCommentTemplate());
+}
+
